91903705e828de8b406a8b45a6818d4544259eac
ilkay777 <ilkay.kaya777gmail.com>
2025-11-14 12:35:04 -0500

    🔧 Modifications du site transframe

---
+++ transframe---transframe/web-files/src-settings-constants.js
@@ -0,0 +1,11 @@
+export const FIXED = {
+  C: {
+    root: '1dd9bdba-d685-ef11-ac21-6045bd5f9b62',     // Code racine principal
+  },
+  J: {
+    root: '80ccc75d-21a3-f011-bbd3-0022483ea92e',     // Job racine initial
+  },
+  T: {
+    folder: '16a5524e-d685-ef11-ac21-6045bd5f9b62',   // Tag représentant un dossier
+  },
+};
